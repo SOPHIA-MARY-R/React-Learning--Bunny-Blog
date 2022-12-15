a3e9dfaca42c4803e88f696600f0a3d29af4675e
SOPHIA MARY R <soph20410.cs@rmkec.ac.in>
2022-12-15 13:22:42 +0530

    Learnt click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,31 @@
 const Home = () => {
+    //The first parameter is a default parameter -> event object
+    /*
+    const handleClick = (e) => {
+        console.log(e);
+    }
+    */
+    const handleClick = () => {
+        console.log('Hello coders!');
+    }
+    const handleClickWithParam = (name) => {
+        console.log('Hello ' + name);
+    }
+    {/*
+    const handleClickWithParam = (name, e) => {
+        console.log('Hello ' + name, e.target);
+    }
+    */}
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Button</button>
+            <button onClick={() => {
+                handleClickWithParam('Sophie!')
+            }}>Name Button</button>
+            {/*
+                <button onClick={(e) => handleClickWithParam('Sophie!', e)}>Name Button</button>
+            */}
         </div>
     );
 }

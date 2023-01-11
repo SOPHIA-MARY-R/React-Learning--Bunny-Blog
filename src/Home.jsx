6ca0f5bb2984972c4067f8efea55882871fd6b0c
SOPHIA MARY R <soph20410.cs@rmkec.ac.in>
2023-01-11 22:51:30 +0530

    Learnt how to use useState hook

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,16 @@
-const Home = () => {
-    let name = 'Sophie';
+import { useState } from "react";
+export default function Home(){
+    const [name, setName]=useState('sophia');
+    //useSate makes the component reactive and updates while re-rendering
+
     const handleClick = () => {
-        name = 'Maria';
+        // setName('maria');
+        if(name==='sophia'){
+            setName('maria');
+        }
+        else{
+            setName('sophia');
+        }
     }
     return (
         <div className="home">
@@ -10,5 +19,3 @@ const Home = () => {
         </div>
     );
 }
- 
-export default Home;

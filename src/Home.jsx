dcd22cb5d511c5817d0aba0650d19c258e8ec348
SOPHIA MARY R <soph20410.cs@rmkec.ac.in>
2023-01-12 00:22:49 +0530

    Reused components for filter & updated style for blogs

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,13 +5,14 @@ export default function Home(){
     const [blogs, setBlogs] = useState([
         {id: 1, topic: 'My summer experience', body: 'The summer was ...', author: 'sophia'},
         {id: 2, topic: 'School days', body: 'My school days were ...', author: 'maria'},
-        {id: 3, topic: 'Rainy days', body: 'I love rain ...', author: 'sophie'}
+        {id: 3, topic: 'Rainy days', body: 'I love rain ...', author: 'sophia'}
     ]);
-    const title="All Blogs";
     return (
         <div className="home">
-            <BlogList blogs={blogs} title={title}/>
+            <BlogList blogs={blogs} title="All blogs"/>
             {/* passing the blogs to blogList using props */}
+            <BlogList blogs={blogs.filter((blog)=> (blog.author === 'sophia'))} title="Sophia's blogs"/>
+            {/* filter only sophia's blogs */}
         </div>
     );
 }

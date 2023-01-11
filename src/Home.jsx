1e94f8d0ee488f018e8abff01a68dab59541966f
SOPHIA MARY R <soph20410.cs@rmkec.ac.in>
2023-01-11 23:32:46 +0530

    Added blog preview

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,21 +1,21 @@
 import { useState } from "react";
 export default function Home(){
-    const [name, setName]=useState('sophia');
-    //useSate makes the component reactive and updates while re-rendering
-
-    const handleClick = () => {
-        // setName('maria');
-        if(name==='sophia'){
-            setName('maria');
-        }
-        else{
-            setName('sophia');
-        }
-    }
+    const [blogs, setBlogs] = useState([
+        {id: 1, topic: 'My summer experience', body: 'The summer was ...', author: 'sophia'},
+        {id: 2, topic: 'School days', body: 'My school days were ...', author: 'maria'},
+        {id: 3, topic: 'Rainy days', body: 'I love rain ...', author: 'sophie'}
+    ]);
     return (
         <div className="home">
-            <p>Hello { name }!</p>
-            <button onClick={handleClick}>Button</button>
+            {blogs.map((blog)=>(
+                <div className="blog-preview" key={blog.id}>
+                    <span className="blog-id">{blog.id}</span>
+                    <h2 className="blog-topic">{blog.topic}</h2>
+                    <h4>Written by {blog.author}</h4>
+                    <p>{blog.body}</p>
+                    <button className="blog-view"><i class="fa fa-eye" aria-hidden="true"></i></button>
+                </div>
+            ))}
         </div>
     );
 }

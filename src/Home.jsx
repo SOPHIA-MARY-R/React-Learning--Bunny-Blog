701c1290430cb3f2558448b3415aba0ad435fc41
SOPHIA MARY R <soph20410.cs@rmkec.ac.in>
2023-01-14 12:37:33 +0530

    Defined a custom & reusable hook

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,52 +1,16 @@
-import { useState, useEffect } from "react";
+import useFetch from "./hooks/useFetch";
 import BlogList from "./components/BlogList";
 
-//JSON Server sets up a fake API
-//to run json-server, go to terminal & type:
-//npx json-server --watch data/db.json --port 8000
-
-//End points:
-/*
-
- http://localhost:8000//blogs             GET         Fetch all blogs
- http://localhost:8000//blogs/{id}        GET         Fetch single blog
- http://localhost:8000//blogs             POST        Add a new blog
- http://localhost:8000//blogs/{id}        DELETE      Delete a blog
-
-*/
-
 export default function Home(){
-    const [blogs, setBlogs] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(()=>{
-        setTimeout(()=>{
-            fetch('http://localhost:8000/blogs')
-            .then(res => {
-                console.log(res);
-                if(!res.ok){ //if response object status is not ok, throw an exception
-                    throw Error('Oops! Could not fetch data from that resource. Please check if the resource exists.');
-                    //for example, trying to fetch from 'http://localhost:8000/blogss' which does not exist
-                }
-                return res.json();
-            })
-            .then((data)=>{
-                setBlogs(data);
-                setIsLoading(false);
-                setError(null);
-            })
-            //catch the error while connecting to JSON server
-            .catch((err)=>{
-                setError(err.message);
-                setIsLoading(false);
-            })
-        }, 1000) //set a timeout for 1s to show Loading ...
-    }, [])
+    const {data, isLoading, error} = useFetch('http://localhost:8000/blogs/');
+    // const {data : blogs, isLoading, error} = useFetch('http://localhost:8000/blogs/');
+    //                 ^
+    //                 |
+    //         get the data in the name 'blogs'
     return (
         <div className="home">
             { error && <div style={{color: 'red'}}>{error}</div>}
-            {isLoading ? <div>Loading ...</div> : (blogs && <BlogList blogs={blogs} title="All blogs"/>)}
+            {isLoading ? <div>Loading ...</div> : (data && <BlogList blogs={data} title="All blogs"/>)}
             {/* call BlogList only if blogs is not null*/}
         </div>
     );
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,37 @@
+import {useState, useEffect} from 'react';
+
+//defining hook in a separate file for reusability
+//the same hook can be used for different resource url and can be reused for fetching different types of data
+//hooks always starts with prefix 'use'
+export default function useFetch(url){
+    const [data, setData] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(()=>{
+        setTimeout(()=>{
+            fetch(url)
+            .then(res => {
+                console.log(res);
+                if(!res.ok){ //if response object status is not ok, throw an exception
+                    throw Error('Oops! Could not fetch data from that resource. Please check if the resource exists.');
+                    //for example, trying to fetch from 'http://localhost:8000/blogss' which does not exist
+                }
+                return res.json();
+            })
+            .then((data)=>{
+                setData(data);
+                setIsLoading(false);
+                setError(null);
+            })
+            //catch the error while connecting to JSON server
+            .catch((err)=>{
+                setError(err.message);
+                setIsLoading(false);
+            })
+        }, 1000) //set a timeout for 1s to show Loading ...
+    }, [url]); //fire useFetch whenever the url changes
+
+    return { data, isLoading, error }
+    //return the above objects to the component which called this
+}

14f5ddc4755d2421921ab9dbd75ee454a0d54ebc
SOPHIA MARY R <soph20410.cs@rmkec.ac.in>
2023-01-14 23:06:22 +0530

    Improvised Creat blog page

--- src/components/CreateBlog.jsx
+++ src/components/CreateBlog.jsx
@@ -22,46 +22,48 @@ export default function CreateBlog(){
     }
 
     return(
-        <div className="blog-container">
-            <h2>Create Blogs here ...</h2>
+        <>
+            <h2>Create your blogs here ...</h2>
             <br/>
-            <div className="blog-edit">
-                <form className="blog-form" onSubmit={handleSubmit}>
-                    <label>Blog Topic </label>
-                    <input 
-                        type="text" 
-                        value={topic} 
-                        required
-                        onChange={(event)=>setTopic(event.target.value)}
-                    />
+            <div className="blog-container">
+                <div className="blog-edit">
+                    <form className="blog-form" onSubmit={handleSubmit}>
+                        <label>Blog Topic </label>
+                        <input 
+                            type="text" 
+                            value={topic} 
+                            required
+                            onChange={(event)=>setTopic(event.target.value)}
+                        />
 
-                    <label>Blog Author </label>
-                    <select
-                        value={author}
-                        onChange={(event)=>setAuthor(event.target.value)}
-                    >
-                        <option value="sophia">sophia</option>
-                        <option value="maria">maria</option>
-                        <option value="jeeva">jeeva</option>
-                    </select>
+                        <label>Blog Author </label>
+                        <select
+                            value={author}
+                            onChange={(event)=>setAuthor(event.target.value)}
+                        >
+                            <option value="sophia">sophia</option>
+                            <option value="maria">maria</option>
+                            <option value="jeeva">jeeva</option>
+                        </select>
 
-                    <label>Blog Body </label>
-                    <textarea 
-                        required
-                        value={body}
-                        onChange={(event)=>setBody(event.target.value)}
-                    />
-                    { !isLoading ? <button className="add-blog">Add</button> : <button className="add-blog-disabled" disabled>Adding ...</button>}
-                </form>
-            </div>
-            <div className="preview">
-                <div className="blog-content">
-                    <h1 className="blog-topic">{topic}</h1>
-                    <p className="blog-author">Written by {author}</p>
-                    <hr/>
-                    <div><p className="blog-body">{body}</p></div>
+                        <label>Blog Body </label>
+                        <textarea 
+                            required
+                            value={body}
+                            onChange={(event)=>setBody(event.target.value)}
+                        />
+                        { !isLoading ? <button className="add-blog">Add</button> : <button className="add-blog-disabled" disabled>Adding ...</button>}
+                    </form>
                 </div>
-            </div>  
-        </div> 
+                <div className="preview">
+                    <div className="blog-content">
+                        <h1 className="blog-topic">{topic}</h1>
+                        <p className="blog-author">Written by {author}</p>
+                        <hr/>
+                        <div><p className="blog-body">{body}</p></div>
+                    </div>
+                </div>  
+            </div> 
+        </>
     );
 }

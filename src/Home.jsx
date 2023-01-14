ed969fe8ff7bcd48583cbf204c2abdd60e8c4327
SOPHIA MARY R <soph20410.cs@rmkec.ac.in>
2023-01-14 11:33:27 +0530

    JSON server setup

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,20 @@
 import { useState, useEffect } from "react";
 import BlogList from "./components/BlogList";
 
+//JSON Server sets up a fake API
+//to run json-server, go to terminal & type:
+//npx json-server --watch data/db.json --port 8000
+
+//End points:
+/*
+
+ http://localhost:8000//blogs             GET         Fetch all blogs
+ http://localhost:8000//blogs/{id}        GET         Fetch single blog
+ http://localhost:8000//blogs             POST        Add a new blog
+ http://localhost:8000//blogs/{id}        DELETE      Delete a blog
+
+*/
+
 export default function Home(){
     const [blogs, setBlogs] = useState([
         {id: 1, topic: 'My summer experience', body: 'The summer was ...', author: 'sophia'},

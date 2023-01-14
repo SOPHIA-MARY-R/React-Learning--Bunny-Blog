f4e5b4e91e2f6f98df9fc7975acbcb800a641e5e
SOPHIA MARY R <soph20410.cs@rmkec.ac.in>
2023-01-14 21:23:07 +0530

    Added create blog page

--- src/components/CreateBlog.jsx
+++ src/components/CreateBlog.jsx
@@ -1,3 +1,51 @@
+import { useState } from "react";
+
 export default function CreateBlog(){
-    return(<h1>Create Blogs here</h1>);
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('sophia');
+
+    return(
+        <div className="blog-container">
+            <h2>Create Blogs here ...</h2>
+            <br/>
+            <div className="blog-edit">
+                <form className="blog-form">
+                    <label>Blog Title </label>
+                    <input 
+                        type="text" 
+                        value={title} 
+                        required
+                        onChange={(event)=>setTitle(event.target.value)}
+                    />
+
+                    <label>Blog Author </label>
+                    <select
+                        value={author}
+                        onChange={(event)=>setAuthor(event.target.value)}
+                    >
+                        <option value="sophia">sophia</option>
+                        <option value="maria">maria</option>
+                        <option value="jeeva">jeeva</option>
+                    </select>
+
+                    <label>Blog Body </label>
+                    <textarea 
+                        required
+                        value={body}
+                        onChange={(event)=>setBody(event.target.value)}
+                    />
+                    <button className="add-blog">Add</button>
+                </form>
+            </div>
+            <div className="preview">
+                <div className="blog-content">
+                    <h1 className="blog-topic">{title}</h1>
+                    <p className="blog-author">Written by {author}</p>
+                    <hr/>
+                    <div><p className="blog-body">{body}</p></div>
+                </div>
+            </div>  
+        </div> 
+    );
 }

de4818500736e3f7eac34a35fb8b071ed6a6a902
SOPHIA MARY R <soph20410.cs@rmkec.ac.in>
2023-01-14 12:12:46 +0530

    Handling errors while connecting with & fetching from server

--- src/Home.jsx
+++ src/Home.jsx
@@ -18,21 +18,34 @@ import BlogList from "./components/BlogList";
 export default function Home(){
     const [blogs, setBlogs] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
     useEffect(()=>{
         setTimeout(()=>{
             fetch('http://localhost:8000/blogs')
             .then(res => {
+                console.log(res);
+                if(!res.ok){ //if response object status is not ok, throw an exception
+                    throw Error('Oops! Could not fetch data from that resource. Please check if the resource exists.');
+                    //for example, trying to fetch from 'http://localhost:8000/blogss' which does not exist
+                }
                 return res.json();
             })
             .then((data)=>{
-                // console.log(data);
                 setBlogs(data);
                 setIsLoading(false);
-            });
+                setError(null);
+            })
+            //catch the error while connecting to JSON server
+            .catch((err)=>{
+                setError(err.message);
+                setIsLoading(false);
+            })
         }, 1000) //set a timeout for 1s to show Loading ...
     }, [])
     return (
         <div className="home">
+            { error && <div style={{color: 'red'}}>{error}</div>}
             {isLoading ? <div>Loading ...</div> : (blogs && <BlogList blogs={blogs} title="All blogs"/>)}
             {/* call BlogList only if blogs is not null*/}
         </div>
